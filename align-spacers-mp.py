f4a57f4870a048ea63856a9a6c7fac1206464680
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2014-08-20 19:59:06 +0200

    Add caching and clean up align-spacers-mp.py

--- align-spacers-mp.py
+++ align-spacers-mp.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2.7
 
 import sys
+import os.path
+import argparse
+
 from Bio import SeqIO
 from Bio.Seq import Seq, reverse_complement
 from Bio.SeqRecord import SeqRecord
@@ -8,15 +11,10 @@ from Bio.Alphabet import generic_dna
 
 from utils import *
 from collections import defaultdict, OrderedDict
-
-import os.path
-
 import traceback
+from multiprocessing import Pool
 
-from multiprocessing import Pool, sharedctypes
-#import matplotlib.pyplot as plt
-import cProfile
-import pstats
+import json
 
 def usage():
 	print "Usage:"
@@ -25,14 +23,12 @@ def usage():
 # found_spacer dict:
 # key: spacer sequence
 # value: (template_name, (same as key in the spacer_db))
-found_spacer_dict = defaultdict(list)
+found_spacer_dict = {}
 
-# spacer_db
-# key: template file name
-# value: dictionary
-#	key: (start, strand, length, mismatch, seq str)
+# spacer_db dictionary
+#	key: (template, start, strand, length, mismatch, [seq str])
 #	value: number of hits
-spacer_db = {}
+spacer_db = defaultdict(int)
 
 class DoubleSeqIterable:
 	def __init__(self, seqh):
@@ -61,10 +57,10 @@ class DoubleSeqIterable:
 			if found_spacer_dict.has_key(str_seq):
 				self.hits += 1
 				self.last_hits += 1
-				spacer_dict_key_list = found_spacer_dict[str_seq]
-				if spacer_dict_key_list[0]:
-					for spacer_dict_key in spacer_dict_key_list[1]:
-						spacer_db[spacer_dict_key[0]][spacer_dict_key[1]] += 1
+				# Sequence does not align -> None
+				if found_spacer_dict[str_seq]:
+					for alignment in found_spacer_dict[str_seq]:
+						spacer_db[tuple(alignment)] += 1
 
 			else:
 				self.misses += 1
@@ -85,7 +81,6 @@ global shared_template_db
 def real_worker_function(arg):
 	seq = arg[0]
 	template_db = shared_template_db
-	n = arg[1]
 
 	match = 0
 	mismatch = 0
@@ -96,22 +91,19 @@ def real_worker_function(arg):
 
 	# Align spacers to the DB sequence, counting the number of occurance
 
-	spacer_db = defaultdict(int)
-	for key in shared_template_db.keys():
-		spacer_db[key] = defaultdict(int)
-
 	found = False
-	for key in shared_template_db.keys():
+	alignments = []
+	for template in shared_template_db.keys():
 		# Try a simple search - it's much faster than alignment.
-		db_seq = shared_template_db[key][0]
-		spacers = spacer_db[key]
+		db_seq = shared_template_db[template][0]
+		db_file_name = shared_template_db[template][1]
 		location = 0
 		while True:
 			location = db_seq.seq.find(seq, location)
 			if location == -1:
 				break
 
-			spacers[(location, 1, len(seq), 0)] += 1
+			alignments.append((template, location, 1, len(seq), 0))
 			found = True
 			location += 1
 			match += 1
@@ -121,7 +113,7 @@ def real_worker_function(arg):
 			if location == -1:
 				break
 
-			spacers[(location, -1, len(seq), 0)] += 1
+			alignments.append((template, location, -1, len(seq), 0))
 			found = True
 			location += 1
 			match += 1
@@ -133,7 +125,7 @@ def real_worker_function(arg):
 		if len(db_seq) > 50000:
 			continue
 
-		score, pos_db, pos_seq = align(seq, str(key) + ".seq")
+		score, pos_db, pos_seq = align(seq, db_file_name + ".seq")
 
 		# Score is 5 * len, -5 for a mismatch, -10 for a gap
 		# Let's allow up to 4 mismatches, or 2 gaps (20 points penalty)
@@ -150,10 +142,10 @@ def real_worker_function(arg):
 			found = True
 			length = pos_db[1] + tail_correction - (pos_db[0] - head_correction)
 
-			spacers[(pos_db[0] - head_correction, 1, length , penalty / 5, seq)] += 1
+			alignments.append((template, pos_db[0] - head_correction, 1, length , penalty / 5, seq))
 
 
-		score, pos_db, pos_seq = align(reverse_complement(seq), str(key) + ".seq")
+		score, pos_db, pos_seq = align(reverse_complement(seq), db_file_name + ".seq")
 		# Score is 5 * len, -5 for a mismatch, -10 for a gap
 		# Let's allow up to 4 mismatches, or 2 gaps (20 points penalty)
 
@@ -169,9 +161,12 @@ def real_worker_function(arg):
 			found = True
 			length = pos_db[1] + tail_correction - (pos_db[0] - head_correction)
 
-			spacers[(pos_db[0] - head_correction, -1, length , penalty / 5, seq)] += 1
+			alignments.append((template, pos_db[0] - head_correction, -1, length , penalty / 5, seq))
 
-	return (found, seq, spacer_db, n)
+		if found:
+			break
+
+	return (found, seq, alignments)
 
 def worker_function(arg):
 
@@ -181,11 +176,11 @@ def worker_function(arg):
 		print "".join(traceback.format_exception(*sys.exc_info()))
 		raise RuntimeError
 
-def gen_tmp_sequence(db_seq, db_fd):
-	name = db_fd.name + ".seq"
+def gen_tmp_sequence(template_seq, template_file):
+	name = template_file + ".seq"
 
 	fh = open(name, "wr+")
-	fh.write(str(db_seq.seq))
+	fh.write(str(template_seq.seq))
 	fh.write("\n")
 	fh.close()
 
@@ -193,16 +188,20 @@ def gen_tmp_sequence(db_seq, db_fd):
 
 def main(argv):
 
-	if (len(argv) < 3):
-		usage()
-		exit(1)
+	parser = argparse.ArgumentParser(description='Align spacers to templates')
+	parser.add_argument('spacers', metavar='<spacer_file.fastq[.gz]>', type=str,
+				help='Input file with PAMs and spacers annotated.')
+	parser.add_argument('templates', metavar='<template.gb>', type=str, nargs='+',
+				help='Template sequences')
+	parser.add_argument('-j', metavar='<out_file.json>', type=str,
+				help='Write statistics to a json file')
+	parser.add_argument('-c', metavar='<cache.json>', type=str,
+				help='Spacer alignment cache file')
+	args = parser.parse_args()
 
-	dbs = []
-	for db in argv[2:]:
-		fh = open(db, "rw")
-		dbs.append(fh)
+	print 'Analyzing', args.spacers
 
-	seqfh = open_maybe_gzip(argv[1])
+	seqfh = open_maybe_gzip(args.spacers)
 	seqh = SeqIO.parse(seqfh, 'fastq', generic_dna)
 
 	# template_db
@@ -215,29 +214,41 @@ def main(argv):
 
 	match = 0
 	mismatch = 0
-	for db_fd in dbs:
-		print "Parsing ", db_fd.name
-		db_seqh = SeqIO.parse(db_fd, "genbank")
-		db_seq = db_seqh.next()
-		db_seqh.close()
+	for template_file in args.templates:
+		template_name = os.path.basename(template_file)
+		print "Parsing ", template_name
 
-		if db_fd.name in template_db.keys():
-			print "Duplicate DB: ", db_name
-			return
+		fd = open(template_file)
+		template_seqh = SeqIO.parse(fd, "genbank")
+		template_seq = template_seqh.next()
+		template_seqh.close()
+		fd.close()
+
+		if template_name in template_db.keys():
+			print "Duplicate DB: ", template_name
+			return 1
 
 		# Remove spacer information from the DB to avoid duplicating it.
 		features = []
-		for feature in db_seq.features:
+		for feature in template_seq.features:
 			if not feature.type == "Spacer":
 				features.append(feature)
 
-		db_seq.features = features
+		template_seq.features = features
 
-		template_db[db_fd.name] = (db_seq, db_fd)
-		spacer_db[db_fd.name] = defaultdict(int)
+		template_db[template_name] = (template_seq, template_file)
 
-		name = gen_tmp_sequence(db_seq, db_fd)
-		template_tmp_seq_list.append(name)
+		temp_name = gen_tmp_sequence(template_seq, template_file)
+		template_tmp_seq_list.append(temp_name)
+
+	if args.c:
+		if os.path.isfile(args.c):
+			cache_f = open(args.c)
+			global found_spacer_dict
+			found_spacer_dict = json.load(cache_f)
+			cache_f.close()
+
+			print 'Loaded %s entries from alignment cache %s' % (len(found_spacer_dict), args.c)
 
 	print "Aligning spacer sequences from %s to %s." % (seqfh.name, ' '.join([x for x in template_db.keys()]))
 
@@ -250,59 +261,63 @@ def main(argv):
 
 	pool = Pool(initializer=share_template_db, initargs=(template_db,))
 
-	n = 0
 	for res in pool.imap(worker_function, iterable, 10):
 		found = res[0]
 		spacer_seq = res[1]
-		spacer_db_update = res[2]
-		n = res[3]
+		alignments = res[2]
 
 		if found:
-			update_list = []
-
-			# For each template
-			for template in spacer_db_update.keys():
-				spacer_dict_update = spacer_db_update[template]
-				# copy the found spacers to the central DB
-				for spacer_key in spacer_dict_update.keys():
-					update_list.append((template, spacer_key))
-
-			for update in update_list:
-				spacer_db[update[0]][update[1]] += 1
-
-			found_spacer_dict[spacer_seq] = (True, update_list)
+			for alignment in alignments:
+				spacer_db[tuple(alignment)] += 1
+			found_spacer_dict[spacer_seq] = alignments
 		else:
-			found_spacer_dict[spacer_seq] = (False, None)
+			found_spacer_dict[spacer_seq] = None
 
-	print "Spacers analyzed: ", n
+
+	print "Spacers analyzed: ", iterable.n
 	print "Cache hits: ", iterable.get_hits()
 	print "Cache misses: ", iterable.get_misses()
 
-	print "Updating the database files..."
+	print "Processing collected data ..."
 
-	# Add spacer annotations to the db sequence
-	for template_key  in template_db.keys():
-		print "Updating %s ..." % template_key
-		db_seq = template_db[template_key][0]
-		db_fd = template_db[template_key][1]
-		db_spacers = spacer_db[template_key]
+	for alignment in spacer_db.keys():
+		template = alignment[0]
+		template_seq = template_db[template][0]
 
-		for spacer_key in db_spacers.keys():
-			qualifiers = { 'hits' : db_spacers[spacer_key],
-				       'len' : spacer_key[2]}
+		hits = spacer_db[alignment]
+		start = alignment[1]
+		strand = alignment[2]
+		length = alignment[3]
 
-			if spacer_key[3]:
-				qualifiers['mismatches'] = spacer_key[3]
-				qualifiers['sequence'] = spacer_key[4]
+		qualifiers = {
+			'hits' : hits,
+			'len' : length
+		}
 
-			db_seq.features.append(SeqFeature(FeatureLocation(spacer_key[0], spacer_key[0] + spacer_key[2]),
-				type="Spacer", strand=spacer_key[1], qualifiers=qualifiers))
-		db_name = db_fd.name
-		db_fd.close()
+		mismatches = alignment[4]
+		if mismatches > 0:
+			sequence = alignment[5]
+			qualifiers['mismatches'] = mismatches
+			qualifiers['sequence'] = sequence
 
-		db_fd = open(db_name, "wr+")
-		SeqIO.write(db_seq, db_fd, "genbank")
-		db_fd.close()
+		template_seq.features.append(SeqFeature(FeatureLocation(start, start+length),
+			type="Spacer", strand=strand, qualifiers=qualifiers))
+
+	for template in template_db.values():
+		template_seq = template[0]
+		template_filename = template[1]
+
+		print "Updating %s ..." % template_filename
+
+		fd = open(template_filename, 'wr+')
+		SeqIO.write(template_seq, fd, 'genbank')
+		fd.close()
+
+	if args.c:
+		print 'Updating alignment cache %s' % args.c
+		cache_f = open(args.c, 'wr+')
+		json.dump(found_spacer_dict, cache_f, sort_keys=True, indent=4)
+		cache_f.close()
 
 	for tmp in template_tmp_seq_list:
 		os.unlink(tmp)
